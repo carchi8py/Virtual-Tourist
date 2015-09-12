13eccd85fd57a951a07690e0cb29d3a99d6b26a5
Chris Archibald <carchi8py@gmail.com>
2015-09-11 20:12:40 -0700

    Ability to create pins.

---
+++ Virtual Tourist/CoreDataStackManager.swift
@@ -0,0 +1,83 @@
+//
+//  CoreDataStackManager.swift
+//  Virtual Tourist
+//
+//  Created by Chris Archibald on 9/11/15.
+//  Copyright (c) 2015 Chris Archibald. All rights reserved.
+//
+
+import CoreData
+
+private let SQLITE_FILE_NAME = "VirtualTourist.sqlite"
+
+class CoreDataStackManager {
+    
+    //MARK: - Shared Instance
+    
+    class func sharedInstance() -> CoreDataStackManager {
+        struct Static {
+            static let instance = CoreDataStackManager()
+        }
+        return Static.instance
+    }
+    
+    //MARK: - Core Data stack
+    
+    lazy var applicationDocumentsDirectory: NSURL = {
+        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        return urls[urls.count-1] as! NSURL
+        
+        }()
+    
+    lazy var managedObjectModel: NSManagedObjectModel = {
+        let modelURL = NSBundle.mainBundle().URLForResource("Virtual_Tourist", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOfURL: modelURL)!
+        }()
+    
+    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
+        var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent(SQLITE_FILE_NAME)
+        
+        var error: NSError? = nil
+        
+        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {
+            coordinator = nil
+            
+            let dict = NSMutableDictionary()
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
+            dict[NSLocalizedFailureReasonErrorKey] = "There was an error creating or loading the application's saved data."
+            dict[NSUnderlyingErrorKey] = error
+            error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict as [NSObject : AnyObject])
+            
+            NSLog("Unresolved error \(error), \(error!.userInfo)")
+            abort()
+        }
+        
+        return coordinator
+        }()
+    
+    lazy var managedObjectContext: NSManagedObjectContext? = {
+        let coordinator = self.persistentStoreCoordinator
+        if coordinator == nil {
+            return nil
+        }
+        var managedObjectContext = NSManagedObjectContext()
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+        }()
+    
+    //MARK: - Core Data saving support
+    
+    func saveContext () {
+        if let context = self.managedObjectContext {
+            
+            var error: NSError? = nil
+            
+            if context.hasChanges && !context.save(&error) {
+                NSLog("Unresolved error \(error), \(error!.userInfo)")
+                abort()
+            }
+        }
+    }
+    
+}
--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController, MKMapViewDelegate {
+class MapViewController: UIViewController, MKMapViewDelegate, UIGestureRecognizerDelegate {
 
     // The Map view object
     @IBOutlet weak var mapView: MKMapView!
@@ -17,11 +17,19 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     //Default to restore map location when app is reopened
     let defaults = NSUserDefaults()
     
+    var pins = [Pin]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Make view delegate the map
         self.mapView.delegate = self
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: "didLongTapMap:")
+        longPress.delegate = self
+        longPress.numberOfTapsRequired = 0
+        longPress.minimumPressDuration = 0.4
+        mapView.addGestureRecognizer(longPress)
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,6 +53,21 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         }
     }
     
+    lazy var sharedContext = {
+        CoreDataStackManager.sharedInstance().managedObjectContext!
+        }()
+    
+    //Pin Functions
+    func addAllPinsToMap() {
+        for pin in pins {
+            var newPin = MKPointAnnotation()
+            newPin.coordinate.latitude = pin.latitude
+            newPin.coordinate.longitude = pin.longitude
+            mapView.addAnnotation(newPin)
+        }
+    }
+    
+    
     //Map View Methods from MKMapViewDeleate
     
     func mapView(mapView: MKMapView!, regionDidChangeAnimated animated: Bool) {
@@ -53,6 +76,40 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         self.defaults.setValue(self.mapView.region.span.latitudeDelta, forKeyPath: "currentLatDelta")
         self.defaults.setValue(self.mapView.region.span.longitudeDelta, forKeyPath: "currentLongDelta")
         println("Saved")
+    }
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        var newPin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "Pin1")
+        newPin.animatesDrop = true
+        
+        return newPin
+    }
+    
+    // Long Press Methods for UIGestureRecognizerDelegate
+    
+    func didLongTapMap(gestureRecognizer: UIGestureRecognizer) {
+        //Get the spot that was tapped.
+        let tapPoint: CGPoint = gestureRecognizer.locationInView(mapView)
+        let touchMapCoordinate: CLLocationCoordinate2D = mapView.convertPoint(tapPoint, toCoordinateFromView: mapView)
+        
+        if gestureRecognizer.state != .Ended {
+            return
+        }
+        
+        let dictionary: [String: AnyObject] = [
+            Pin.Keys.Latitude: touchMapCoordinate.latitude,
+            Pin.Keys.Longitude: touchMapCoordinate.longitude
+        ]
+        
+        //Create the pin to be added
+        let pinToBeAdded = Pin(dictionary: dictionary, context: self.sharedContext)
+        
+        self.pins.append(pinToBeAdded)
+        //Add the pin to mapView
+        var dropPin = MKPointAnnotation()
+        dropPin.coordinate.latitude = pinToBeAdded.latitude
+        dropPin.coordinate.longitude = pinToBeAdded.longitude
+        mapView.addAnnotation(dropPin)
     }
 }
 
