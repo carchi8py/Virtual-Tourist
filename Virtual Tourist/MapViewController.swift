e98498785b17e2e187c48638a3c6c2869cfd92ba
Chris Archibald <carchi8py@gmail.com>
2015-09-11 19:13:03 -0700

    Code to save the location on the map, and then restore it when the app restarts.

--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
+    // The Map view object
     @IBOutlet weak var mapView: MKMapView!
+    
+    //Default to restore map location when app is reopened
+    let defaults = NSUserDefaults()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.mapView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -22,7 +28,30 @@ class MapViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewWillAppear(animated: Bool) {
+        if let currentLatitude = self.defaults.valueForKey("currentLatitude") as? Double {
+            let currentLongitude: Double = self.defaults.valueForKey("currentLongitude") as! Double
+            let currentLatDelta: Double = self.defaults.valueForKey("currentLatDelta") as! Double
+            let currentLongDelta: Double = self.defaults.valueForKey("currentLongDelta") as! Double
+            
+            let centerCoordinate = CLLocationCoordinate2D(latitude: currentLatitude, longitude: currentLongitude)
+            let span = MKCoordinateSpan(latitudeDelta: currentLatDelta, longitudeDelta: currentLongDelta)
+            let currentRegion = MKCoordinateRegionMake(centerCoordinate, span)
+            println("restore")
+            
+            self.mapView.setRegion(currentRegion, animated: false)
+        }
+    }
+    
+    //Map View Methods from MKMapViewDeleate
+    
+    func mapView(mapView: MKMapView!, regionDidChangeAnimated animated: Bool) {
+        self.defaults.setValue(self.mapView.region.center.latitude, forKeyPath: "currentLatitude")
+        self.defaults.setValue(self.mapView.region.center.longitude, forKeyPath: "currentLongitude")
+        self.defaults.setValue(self.mapView.region.span.latitudeDelta, forKeyPath: "currentLatDelta")
+        self.defaults.setValue(self.mapView.region.span.longitudeDelta, forKeyPath: "currentLongDelta")
+        println("Saved")
+    }
 }
 
