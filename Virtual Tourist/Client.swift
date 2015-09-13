a3e3b3003215b3b6519d6d236fbe12bbefb9ec85
Chris Archibald <carchi8py@gmail.com>
2015-09-12 19:17:02 -0700

    Adding out helper function from pervious projects.

--- Virtual Tourist/Client.swift
+++ Virtual Tourist/Client.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+
+class Client: NSObject {
+    
+    // Shared Session
+    var session: NSURLSession
+    
+    override init() {
+        session = NSURLSession.sharedSession()
+        super.init()
+    }
+    
+    
+    
+    /***** Helper Functions *****/
+    
+    /* Helper: Given raw JSON, return a usable Foundation object */
+    class func parseJSONWithCompletionHandler(data: NSData, completionHandler: (result: AnyObject!, error: NSError?) -> Void) {
+        
+        var parsingError: NSError? = nil
+        
+        let parsedResult: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError)
+        
+        if let error = parsingError {
+            print("Error in json")
+            completionHandler(result: nil, error: error)
+        } else {
+            print("We are good in json")
+            completionHandler(result: parsedResult, error: nil)
+        }
+    }
+    
+    /* Helper function: Given a dictionary of parameters, convert to a string for a url */
+    class func escapedParameters(parameters: [String : AnyObject]) -> String {
+        
+        var urlVars = [String]()
+        
+        for (key, value) in parameters {
+            
+            /* Make sure that it is a string value */
+            let stringValue = "\(value)"
+            
+            /* Escape it */
+            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+            
+            /* Append it */
+            urlVars += [key + "=" + "\(escapedValue!)"]
+            
+        }
+        
+        return (!urlVars.isEmpty ? "?" : "") + join("&", urlVars)
+    }
+    
+    //The shared Instance
+    class func sharedInstance() -> Client {
+        struct Singleton {
+            static var sharedInstance = Client()
+        }
+        return Singleton.sharedInstance
+    }
+}
