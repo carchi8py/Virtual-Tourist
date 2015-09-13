e18075a0a33d43c3c21f4a80377cc8f7638e8563
Chris Archibald <carchi8py@gmail.com>
2015-09-12 17:04:21 -0700

    Created zoom in on pin on Photo Page

--- Virtual Tourist/PhotoViewController.swift
+++ Virtual Tourist/PhotoViewController.swift
@@ -7,9 +7,33 @@
 //
 
 import UIKit
+import MapKit
 import Foundation
 
 class PhotoViewController: UIViewController {
     var pin: Pin!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Passes in the vaule of that was selected on the Map View Page
+        setMapLocation(pin.latitude, longitude: pin.longitude)
+    }
+    
+    
+    /***** Map Function *****/
+    
+    //Set the location of the map to the selected Pin from the Map View Page
+    func setMapLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: latitude, longitude: longitude), span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.01))
+        
+        let newPin = MKPointAnnotation()
+        newPin.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        mapView.addAnnotation(newPin)
+        mapView.setRegion(region, animated: true)
+    }
     
 }
