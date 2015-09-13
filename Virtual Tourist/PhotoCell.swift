dc00822a147069bc336b7a57accf3c564f54dc8a
Chris Archibald <carchi8py@gmail.com>
2015-09-12 17:21:21 -0700

    Set up and image cell (i think)

---
+++ Virtual Tourist/PhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCell.swift
+//  Virtual Tourist
+//
+//  Created by Chris Archibald on 9/12/15.
+//  Copyright (c) 2015 Chris Archibald. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var imageActivity: UIActivityIndicatorView!
+}
